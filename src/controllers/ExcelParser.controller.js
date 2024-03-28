915b884201c759a5f05813ea1e1187a051c9e504
Yuvraj Singh <yuvi7860808034@gmail.com>
2024-03-28 13:59:55 +0530

    download and upload api added

--- src/controllers/ExcelParser.controller.js
+++ src/controllers/ExcelParser.controller.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
 const excelParserServices = require("../services/ExcelParser.services");
 
+// Multer storage configuration
+const storage = multer.memoryStorage(); // Store files in memory
+
+// Multer instance
+const upload = multer({ storage: storage });
+
 router.get("/read", readExcel);
+router.get("/downloadFormTemplate", downloadFormTemplate);
+router.post("/uploadFormTemplate", upload.single("file"), uploadFormTemplate); // Handle file upload
 
 module.exports = router;
 
@@ -22,5 +31,46 @@ async function readExcel(req, res, next) {
     res
       .status(500)
       .json({ message: error?.message || "Error reading Excel file." });
+  }
+}
+
+async function downloadFormTemplate(req, res, next) {
+  try {
+    const fileName = process.env.FORM_TEMPLATE_FILENAME;
+
+    if (!fileName) {
+      throw new Error("File Name not found in request parameters.");
+    }
+
+    const fileData = await excelParserServices.downloadFormTemplate(fileName);
+
+    // Set the content type and attachment disposition for download
+    res.setHeader("Content-Type", "application/octet-stream");
+    res.setHeader("Content-Disposition", `attachment; filename="${fileName}"`);
+
+    // Send the file data as response
+    res.send(fileData);
+  } catch (error) {
+    console.error("Error downloading file:", error);
+    res
+      .status(500)
+      .json({ message: error.message || "Error downloading file." });
+  }
+}
+
+async function uploadFormTemplate(req, res, next) {
+  try {
+    if (!req.file) {
+      throw new Error("No file uploaded.");
+    }
+
+    // Read Excel file directly from memory
+    const bufferData = req.file.buffer;
+    const data = await excelParserServices.parseExcelBuffer(bufferData);
+
+    res.status(200).json({ data });
+  } catch (error) {
+    console.error("Error uploading file:", error);
+    res.status(500).json({ message: error.message || "Error uploading file." });
   }
 }
--- src/services/ExcelParser.services.js
+++ src/services/ExcelParser.services.js
@@ -28,6 +28,52 @@ async function readExcelFile(fileName) {
   return allRows;
 }
 
+async function downloadFormTemplate(fileName) {
+  return new Promise((resolve, reject) => {
+    // Assuming the assets folder path
+    const filePath = path.join(__dirname, "../../assets", fileName);
+
+    // Check if the file exists
+    if (!fs.existsSync(filePath)) {
+      reject(new Error("File not found."));
+    }
+
+    // Read the file asynchronously
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        reject(err);
+      }
+
+      resolve(data);
+    });
+  });
+}
+
+async function parseExcelBuffer(buffer) {
+  try {
+    // Load Excel workbook from buffer
+    const workbook = xlsx.read(buffer, { type: "buffer" });
+
+    // Assuming the first sheet is the one you want to read
+    const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+
+    // Extract data from the worksheet
+    const allRows = xlsx.utils.sheet_to_json(worksheet);
+
+    allRows.forEach((row) => {
+      if (row?.Option) {
+        row.Option = row.Option.split(",");
+      }
+    });
+
+    return allRows;
+  } catch (error) {
+    throw new Error("Error parsing Excel data from buffer: " + error.message);
+  }
+}
+
 module.exports = {
   readExcelFile,
+  downloadFormTemplate,
+  parseExcelBuffer,
 };
